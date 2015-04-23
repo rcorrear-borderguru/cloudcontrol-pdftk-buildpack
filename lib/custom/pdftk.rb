3b78fc829f7923d00dae6a951c48d5ae797bab9a
jorn.wanke <j.wanke@borderguru.com>
2015-04-23 16:31:09 +0200

    more more more

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -26,6 +26,7 @@ class Pdftk < BaseCustom
     %x{ cd #{build_path} && ls #{build_path} 1>&2 }
     
     %x{ mkdir -p #{build_path}/bin }
+    %x{ mkdir -p #{build_path}/lib }
     %x{ mv #{path}/bin/pdftk #{build_path}/bin/pdftk } 
     %x{ mv #{path}/lib/libgcj.so.12 #{build_path}/lib/libgcj.so.12 } 
     write_stdout "complete compiling #{name}"
