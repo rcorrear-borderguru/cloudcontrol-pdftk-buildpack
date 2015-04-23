3f838ef8262289a66c6eae86f236cf61463a504f
jorn.wanke <j.wanke@borderguru.com>
2015-04-23 17:53:51 +0200

    .\lib

--- libpath.sh
+++ libpath.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/srv/tmp/builddir/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/srv/tmp/builddir/lib
 echo "new library path"
 echo $LD_LIBRARY_PATH
