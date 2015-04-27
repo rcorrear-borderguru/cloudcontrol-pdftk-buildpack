8615669c2bde15be0edc6a5523b3f574ff74631f
Ricardo Correa <r.correa@borderguru.com>
2015-04-27 14:27:47 +0200

    Update pdftk.rb

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -44,7 +44,7 @@ class Pdftk < BaseCustom
     %x{ cp #{path}/lib/libgcj.so.12 #{build_path}/lib/libgcj.so.12 } 
 
     %x{ mkdir -p #{profile} }
-    p = File.expand_path(#{profile}/pdftk.sh)
+    p = File.expand_path("#{profile}/pdftk.sh")
     write_stdout(p)
     f = File.new(p, "w")
     f.write(#{paths})
