78d0628662ba8106297cdb9fb2d418cc8ada2a53
Ricardo Correa <r.correa@borderguru.com>
2015-04-27 12:01:14 +0200

    Update pdftk.rb
    Removed a lot of unnecessary code from the "compile" script

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -21,10 +21,6 @@ class Pdftk < BaseCustom
     "${HOME}/.profile.d"
   end
   
-  def build_path_profile
-    "#{build_path}/.profile.d"
-  end
-
   def used?
     File.exist?("#{build_path}/bin/pdftk") && File.exist?("#{build_path}/bin/lib/libgcj.so.12")
   end
@@ -33,26 +29,14 @@ class Pdftk < BaseCustom
     write_stdout "compiling #{name} using #{path} and #{build_path}"
     #download the source and extract
     %x{ mkdir -p #{path} && curl --silent #{source_url} -o - | tar -xz -C #{path} -f - }
-    %x{ cd #{path} && ls -R #{path} 1>&2 }
-    write_stdout "--------------buildpath:"
-    %x{ cd #{build_path} && ls #{build_path} 1>&2 }
-    
+
     %x{ mkdir -p #{build_path}/bin }
-    %x{ mkdir -p #{build_path}/bin/lib }
     %x{ mkdir -p #{build_path}/lib }
     %x{ cp #{path}/bin/pdftk #{build_path}/bin/pdftk } 
     %x{ cp #{path}/lib/libgcj.so.12 #{build_path}/lib/libgcj.so.12 } 
-    %x{ cp #{path}/lib/libgcj.so.12 #{build_path}/bin/libgcj.so.12 } 
-    %x{ cp #{path}/lib/libgcj.so.12 #{build_path}/bin/lib/libgcj.so.12 } 
 
-    write_stdout "profile #{profile}"
-    %x{ echo #{profile} 1>&2 }
-    %x{ echo #{build_path_profile} 1>&2 }
-    %x{ mkdir -p #{profile} && curl -L #{shell_script_url} -o - > /srv/www/.profile.d/pdftk.sh }
-    %x{ mkdir -p #{build_path_profile} }
-    %x{ cp #{profile}/pdftk.sh #{build_path_profile} }
-    %x{ cat #{profile}/pdftk.sh 1>&2 }
-    
+    %x{ mkdir -p #{profile} && curl -L #{shell_script_url} -o - > #{profile}/pdftk.sh }
+
     write_stdout "complete compiling #{name}"
   end
 
@@ -60,8 +44,8 @@ class Pdftk < BaseCustom
   end
 
   def prepare
-    #File.delete("#{build_path}/bin/lib/libgcj.so.12") if File.exist?("#{build_path}/bin/libgcj.so.12")
-    #File.delete("#{build_path}/bin/pdftk") if File.exist?("#{build_path}/bin/pdftk")
+    File.delete("#{build_path}/bin/lib/libgcj.so.12") if File.exist?("#{build_path}/bin/libgcj.so.12")
+    File.delete("#{build_path}/bin/pdftk") if File.exist?("#{build_path}/bin/pdftk")
   end
 
 end
